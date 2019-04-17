f195676cad90ebfc0e38d8bffc0e681ad766fb47
GrinOleksandr <sasha.lisove@gmail.com>
2019-04-17 13:01:10 +0300

    fixing deploy version

--- config.js
+++ config.js
@@ -2,6 +2,6 @@
 
 module.exports = {
   // ip: '127.0.0.1',
-  productStorage:'http://wovenly-server.herokuapp.com',
+  productStorage:'https://wovenly-server.herokuapp.com',
   port: process.env.PORT || 8000
 }
